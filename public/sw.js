f447e0f6af12ffe306f56fe465d93b4aee6b7923
oomiapp25 <jjtovar1510@gmail.com>
2026-03-21 19:39:36 -0400

    Refactor service worker caching logic

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,22 @@
-const CACHE_NAME = 'jx4-cache-v1';
-const OFFLINE_URL = '/offline.html';
-
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'jx4-v1';
+const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json',
-  '/offline.html'
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(ASSETS);
     })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
